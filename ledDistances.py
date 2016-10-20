9b1e08118f984ed658b8b89675fe51fb7db604fc
Diego Aguilera <diego.aguilera@variableinc.com>
2016-10-19 23:06:20 -0400

    Added script to calculate LED positions

---
+++ ledDistances.py
@@ -0,0 +1,19 @@
+#Calculate LED distances
+
+import math
+import numpy as np
+
+yOffset = 1.03
+xOffset = 1.02
+
+l = 0.87
+
+numOfLeds = 12
+
+ledPositions = np.arange(numOfLeds)
+
+for i in range(0, numOfLeds):
+    xPos = ( l * math.cos( math.radians( ledPositions[i] * 30 ) ) ) + xOffset
+    yPos = ( l * math.sin( math.radians( ledPositions[i] * 30 ) ) ) + yOffset
+
+    print ("%i - %4.3f - %4.3f") %(i, yPos, xPos)
